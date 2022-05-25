aaba51f90104253d8f91369f803592b95ca6ee4c
tboonma <tboonma@hotmail.com>
2022-05-25 21:16:21 +0700

    Change title and favicon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../assets/styles/styles.css'
 import BalanceCard from '../components/BalanceCard.js'
 import SpendingCard from '../components/SpendingCard'
 
 const Home = () => {
+  useEffect(() => {
+    document.title = 'Expenses chart'
+  }, [])
   return (
     <div className="w-screen h-screen grid place-items-center">
       <div className="components-width grid gap-6 max-w-lg">
